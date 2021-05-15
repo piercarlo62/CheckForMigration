8f902e95fbde61b16fc0826f171aae4bb9a4b514
piercarlo schiavo <piercarlo.schiavo@gmail.com>
2021-05-15 08:21:10 +0200

    Only Model For Migration

---
+++ CheckForMigration/Server/Migrations/20210515055813_TestModelsMigration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CheckForMigration.Server.Migrations
+{
+    public partial class TestModelsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PropString = table.Column<string>(type: "text", nullable: true),
+                    PropStringArray = table.Column<string[]>(type: "text[]", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestModels", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestModels");
+        }
+    }
+}
---
+++ CheckForMigration/Server/Models/TestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CheckForMigration.Server.Models
+{
+    public class TestModel
+    {
+        public int Id { get; set; }
+        public string PropString { get; set; }
+        public string[] PropStringArray { get; set; }
+    }
+}
